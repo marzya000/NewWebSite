2e3e82d01038fcce441b9006f53d7a4d528e19ba
Marzya <mnaq_1987@yahoo.com>
2026-06-16 20:25:50 +0330

    Adding is_verified

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -5,8 +5,8 @@ from .models import User, Profile
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email','is_superuser','is_active')
-    list_filter = ('email','is_superuser','is_active')
+    list_display = ('email','is_superuser','is_active','is_verified')
+    list_filter = ('email','is_superuser','is_active','is_verified')
     searching_fields = ('email',)
     ordering = ('email',)
 
@@ -18,7 +18,7 @@ class CustomUserAdmin(UserAdmin):
         }),
         ('Permissions', {
             "fields": (
-                'is_staff','is_active','is_superuser'
+                'is_staff','is_active','is_superuser','is_verified'
             ),
         }),
         ('group Permissions', {
@@ -36,7 +36,7 @@ class CustomUserAdmin(UserAdmin):
         (None, {
             "classes": ("wide",),
             "fields": (
-               "email", "password1","password2", "is_staff", "is_active","is_superuser"
+               "email", "password1","password2", "is_staff", "is_active","is_superuser","is_verified"
                 )
                 }),
     )

03fa0b278dc460febdb28609419088c59b4aed36
Marzya <mnaq_1987@yahoo.com>
2026-06-20 12:45:35 +0330

    Build the required elements before starting-chapter-10

--- core/blog/tests/test_blog_model.py
+++ core/blog/tests/test_blog_model.py
@@ -5,21 +5,23 @@ from django.contrib.auth import get_user_model
 from accounts.models import User, Profile
 
 class TestPostModel(TestCase):
-
-    def test_create_post_with_valid_data(self):
-        user = User.objects.create_user(email="test@test.com",password="@#123456")
-        profile = Profile.objects.create(
-            user = user,
+    def setUp(self):
+        self.user = User.objects.create_user(email="test@test.com",password="@#123456")
+        self.profile = Profile.objects.create(
+            user = self.user,
             first_name = "test_first_name",
             last_name = "test_last_name",
             description = "test description",            
             )
+    def test_create_post_with_valid_data(self):
+        
         post = Post.objects.create(
-            author = user,
+            author = self.user, 
             title ="test",
             content ="description",
-            status = True,
+            status = True,           
             category = None,
             published_date = datetime.now()
         )
-        self.assertEquals(post.title, "test")
+        self.assertTrue(Post.objects.filter(pk=post.id).exists())
+        self.assertEquals(post.title,"test")

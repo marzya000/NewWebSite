59efd52e1e90b1f8cfeee08dd9fdf4a90ca656cb
Marzya <mnaq_1987@yahoo.com>
2026-06-23 20:36:25 +0330

    Introduction to CorsHeader and settings_chapter-13

--- core/core/settings.py
+++ core/core/settings.py
@@ -55,11 +55,13 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "mail_templated",
     "djoser",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -193,3 +195,8 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "LEEWAY": 30,
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500"
+]

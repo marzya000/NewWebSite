9c21564b45fec66e25e560b96ab2cf0450885213
Marzya <mnaq_1987@yahoo.com>
2026-06-24 13:36:15 +0330

    Update locustfile.py and Request with user using jwt_chapter-14

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -1,11 +1,16 @@
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
-    
+    def on_start(self):
+        response = self.client.post('/accounts/api/v1/jwt/create/',data={
+            "email": "admin@admin.com",
+            "password": "@#123456"
+        }).json()
+        self.client.headers = {'Authorization': f"Bearer {response.get('access',None)}"}
+
     @task
     def post_list(self):
-        self.client.get("/blog/api/v1/post/")
-        
+        self.client.get("/blog/api/v1/post/")        
 
     @task
     def post_category(self):
